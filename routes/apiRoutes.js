e8e0d0c74b226b78ba8856e53a84db78239ac9f6
Rutu Thakkar <rutu.thakkar@iviewlabs.net>
2022-01-11 11:06:47 +0530

    routes uncommented

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,9 +6,9 @@ const db = require('../models');
 const userController = require('../controller/userController')
 
 
-// route.post('/signup', userController.signup)
-// route.post('/signin', userController.login)
+route.post('/signup', userController.signup)
+route.post('/signin', userController.login)
 
-route.get('/', userController.tokenfunc)
+// route.get('/', userController.tokenfunc)
 
 module.exports = route;
